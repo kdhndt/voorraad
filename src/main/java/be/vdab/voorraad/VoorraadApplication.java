054db1f332a88502d9b1a468e06f8c2cba9eaa93
Kasper Dehondt <kasper.dehondt@vdabcampus.be>
2022-03-11 13:42:02 +0100

    Initial commit

---
+++ src/main/java/be/vdab/voorraad/VoorraadApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.voorraad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoorraadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoorraadApplication.class, args);
+    }
+
+}
